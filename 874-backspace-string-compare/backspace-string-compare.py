0cf8036a99f1063b364a5965a49042f4d6ddc509
Ankit Thapar <120432770+Ankit16727@users.noreply.github.com>
2025-09-06 15:08:32 -0400

    Time: 5 ms (5.51%) | Memory: 12.5 MB (52.16%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -6,6 +6,8 @@ class Solution(object):
         :rtype: bool
         """
         # Understand the question correctly
+        # O(n) -> space complexity.
+        """
         stack_s, stack_t = [], []
         for i in range(len(s)):
             if s[i] == '#':
@@ -24,6 +26,46 @@ class Solution(object):
         print(stack_s, stack_t)
         
         return stack_s == stack_t
+        """
+        # 2 pointer approach
+        # O(1) -> extra space complexity
+        si, ti, ns, nt = len(s) - 1, len(t) - 1, 0, 0
+        while si >= 0 or ti >= 0:
+            if si >= 0 and ti >= 0:
+                if s[si] != '#' and t[ti] != '#' and ns == 0 and nt == 0:
+                    if s[si] != t[ti]:
+                        return False
+                    si -= 1
+                    ti -= 1
+                
+            if si >= 0:
+                if s[si] == '#':
+                    ns += 1
+                    si -= 1
+                else:
+                    if ns > 0:
+                        ns -= 1
+                        si -= 1
+                    elif ti < 0:
+                        return False
+                
+            if ti >= 0:
+                if t[ti] == '#':
+                    nt += 1
+                    ti -= 1
+                else:
+                    if nt > 0:
+                        nt -= 1
+                        ti -= 1
+                    elif si < 0:
+                        return False
+        return True
+        
+        
+            
+            
+            
+            
             
             
             
