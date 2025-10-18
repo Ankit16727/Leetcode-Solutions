8b7b2f716f9b180ca65ab29e21a32118aab269a2
Ankit Thapar <120432770+Ankit16727@users.noreply.github.com>
2025-10-18 16:43:35 -0400

    Time: 225 ms (15.00%) | Memory: 15.1 MB (97.50%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minSwaps(self, nums):
+        windowSize = nums.count(1)
+        l, r, size, cnt1, minSwaps = 0, 0, 0, 0, None
+
+        if windowSize == 0:
+            return 0
+        else:
+            while l != len(nums):
+                size += 1
+                if nums[r] == 1:
+                    cnt1 += 1
+                if size == windowSize:
+                    minSwaps = windowSize - cnt1 if minSwaps == None else min(minSwaps, windowSize - cnt1)
+                    if nums[l] == 1:
+                        cnt1 -= 1
+                    l += 1
+                    size -= 1
+                if r == len(nums) - 1:
+                    r = -1
+                r += 1
+            
+            return minSwaps
+
+        
